df60075c3025a17842681a15e04b345e0cf32b34
Sameer Thalappil <sameert@codeaurora.org>
2013-10-16 14:41:21 -0700

    Clear the scan list when an empty list is received
    When the framework receives an empty scan list from supplicant, it does
    not clear the local list. So the UI will continue to show the previous
    scan list even when there are no APs around. So clear the local list,
    when an empty list is received.
    
    CRs-Fixed: 552212
    Change-Id: I8715bc31ce0df7bd54c96fcf7d7b73592429f12a

--- wifi/java/android/net/wifi/WifiStateMachine.java
+++ wifi/java/android/net/wifi/WifiStateMachine.java
@@ -1948,6 +1948,7 @@ public class WifiStateMachine extends StateMachine {
 
         scanResults = scanResultsBuf.toString();
         if (TextUtils.isEmpty(scanResults)) {
+           mScanResults.clear();
            return;
         }
 
