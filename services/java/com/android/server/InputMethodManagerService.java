5e3a18ac816a6ba0ad6ee9075bea923a79cfd5c9
David96 <xd_e-mail@gmx.de>
2013-12-26 18:18:44 +0100

    Fix status bar ime switcher setting not sticking on reboot

--- services/java/com/android/server/InputMethodManagerService.java
+++ services/java/com/android/server/InputMethodManagerService.java
@@ -833,11 +833,14 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
                 mStatusBar = statusBar;
                 statusBar.setIconVisibility("ime", false);
                 updateImeWindowStatusLocked();
-                mShowOngoingImeSwitcherForPhones = mRes.getBoolean(
-                        com.android.internal.R.bool.show_ongoing_ime_switcher);
-                if (mShowOngoingImeSwitcherForPhones) {
-                    mWindowManagerService.setOnHardKeyboardStatusChangeListener(
-                            mHardKeyboardListener);
+                if (Settings.System.getIntForUser(mContext.getContentResolver(), 
+                            Settings.System.STATUS_BAR_IME_SWITCHER, UserHandle.USER_CURRENT, 1) == 1) {
+                    mShowOngoingImeSwitcherForPhones = mRes.getBoolean(
+                            com.android.internal.R.bool.show_ongoing_ime_switcher);
+                    if (mShowOngoingImeSwitcherForPhones) {
+                        mWindowManagerService.setOnHardKeyboardStatusChangeListener(
+                                mHardKeyboardListener);
+                    }
                 }
                 buildInputMethodListLocked(mMethodList, mMethodMap,
                         !mImeSelectedOnBoot /* resetDefaultEnabledIme */);
