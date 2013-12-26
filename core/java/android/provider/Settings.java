7350e91b42e7fe7397c5539cebe4c0ece0f9ea00
David96 <xd_e-mail@gmx.de>
2013-12-26 16:26:39 +0100

    Lockscreen notifications: make height user configurable [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2708,6 +2708,12 @@ public final class Settings {
         public static final String LOCKSCREEN_NOTIFICATIONS_WAKE_ON_NOTIFICATION = "lockscreen_notifications_wake_on_notification";
 
         /**
+         * Sets the count of notifications shown at once
+         * @hide
+         */
+        public static final String LOCKSCREEN_NOTIFICATIONS_HEIGHT = "lockscreen_notifications_height";
+
+        /**
          * MediaScanner behavior on boot.
          * 0 = enabled
          * 1 = ask (notification)
