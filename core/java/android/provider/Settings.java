f5ef960b61b176cbbcd72a79298f4f9ac3c00b17
David96 <xd_e-mail@gmx.de>
2014-01-05 02:08:10 +0100

    Reverse default app picker behaviour [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2438,6 +2438,12 @@ public final class Settings {
         public static final String POINTER_LOCATION = "pointer_location";
 
         /**
+         * Reverse default app picker behaviour
+         * @hide
+         */
+        public static final String REVERSE_DEFAULT_APP_PICKER = "reverse_default_app_picker";
+
+        /**
          * Show touch positions on screen?
          * 0 = no
          * 1 = yes
