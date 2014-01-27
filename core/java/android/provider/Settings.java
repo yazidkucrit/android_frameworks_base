0ef99d468cd9fec20af620a2413ad96437e93f97
0xD34D <clark@scheffsblend.com>
2014-01-27 00:09:43 -0600

    Gesture Lockscreen [1/2]
    Change-Id: I370d40d47459d64adf709d4caee1c27c75922cfd
    
    This commit is more "original" than previous ones.
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/java/com/android/internal/widget/LockPatternUtils.java
    	services/java/com/android/server/LockSettingsService.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3556,6 +3556,8 @@ public final class Settings {
             MOVED_TO_LOCK_SETTINGS.add(Secure.LOCK_PATTERN_ENABLED);
             MOVED_TO_LOCK_SETTINGS.add(Secure.LOCK_PATTERN_VISIBLE);
             MOVED_TO_LOCK_SETTINGS.add(Secure.LOCK_PATTERN_TACTILE_FEEDBACK_ENABLED);
+            MOVED_TO_LOCK_SETTINGS.add(Secure.LOCK_GESTURE_ENABLED);
+            MOVED_TO_LOCK_SETTINGS.add(Secure.LOCK_GESTURE_VISIBLE);
 
             MOVED_TO_GLOBAL = new HashSet<String>();
             MOVED_TO_GLOBAL.add(Settings.Global.ADB_ENABLED);
@@ -4276,6 +4278,12 @@ public final class Settings {
         public static final String LOCK_BEFORE_UNLOCK = "lock_before_unlock";
 
         /**
+         * Determines the width and height of the LockPatternView widget
+         * @hide
+         */
+        public static final String LOCK_PATTERN_SIZE = "lock_pattern_size";
+
+        /**
          * Whether lock pattern will vibrate as user enters (0 = false, 1 =
          * true)
          *
@@ -4286,6 +4294,18 @@ public final class Settings {
         @Deprecated
         public static final String
                 LOCK_PATTERN_TACTILE_FEEDBACK_ENABLED = "lock_pattern_tactile_feedback_enabled";
+
+        /**
+         * Whether autolock is enabled (0 = false, 1 = true)
+         * @hide
+         */
+        public static final String LOCK_GESTURE_ENABLED = "lock_gesture_autolock";
+
+        /**
+         * Whether lock gesture is visible as user enters (0 = false, 1 = true)
+         * @hide
+         */
+        public static final String LOCK_GESTURE_VISIBLE = "lock_gesture_visible_pattern";
 
         /**
          * This preference allows the device to be locked given time after screen goes off,
