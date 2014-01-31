564b642857ac467d98cc2cc2ec480182e324b5c0
David96 <xd_e-mail@gmx.de>
2014-02-01 00:17:37 +0100

    Custom lockscreen wallpaper [1/2]

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewMediator.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewMediator.java
@@ -546,6 +546,10 @@ public class KeyguardViewMediator {
         mKeyguardViewManager.setBackgroundBitmap(bmp);
     }
 
+    public void setWallpaper(Bitmap bmp) {
+        mKeyguardViewManager.setWallpaper(bmp);
+    }
+
     /**
      * Let us know that the system is ready after startup.
      */
@@ -1162,7 +1166,7 @@ public class KeyguardViewMediator {
     protected void handleDispatchButtonClickEvent(int buttonId) {
         mKeyguardViewManager.dispatchButtonClick(buttonId);
     }
-    
+
     private void sendUserPresentBroadcast() {
         final UserHandle currentUser = new UserHandle(mLockPatternUtils.getCurrentUser());
         mContext.sendBroadcastAsUser(USER_PRESENT_INTENT, currentUser);
@@ -1405,7 +1409,7 @@ public class KeyguardViewMediator {
         msg.arg1 = buttonId;
         mHandler.sendMessage(msg);
     }
-    
+
     public void launchCamera() {
         Message msg = mHandler.obtainMessage(LAUNCH_CAMERA);
         mHandler.sendMessage(msg);
