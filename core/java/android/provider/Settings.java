78625058cbb6ba267ef68920db62ab248b2d7b94
TheMrcool212 <munirmohamadrani@gmail.com>
2014-02-05 20:38:00 +0800

    Revert "Screen recorder: add ability to record audio from mic [1/2]"
    This reverts commit 887f7183c34b8fc77c0a8dbb140585c98d152f38.
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3410,12 +3410,6 @@ public final class Settings {
          */
         public static final String TELO_RADIO_HIGH_POWER = "telo_radio_high_power";
 
-       /**
-         * Whether to include audio when recording a video
-         * @hide
-         */
-        public static final String SCREEN_RECORDER_RECORD_AUDIO = "screen_recorder_record_audio";
-
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
