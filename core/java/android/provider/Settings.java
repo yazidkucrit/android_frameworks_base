01277fac92c924aee3b6491d38a4b44a9461add4
hany <hany.alsamman@gmail.com>
2014-02-24 03:19:35 +0000

    XuiMod: Toast Animations [1/2]
    Add Xylon, Xperia Z1, TokoROM Transition and more
    
    https://github.com/zst123/XuiMod/commit/8fc1152fb485ed5e007ac8d4ea9ad7b7f1c493d8
    
    https://github.com/zst123/XuiMod/commit/1989a90850e5491799620e1f86fcbc9ae54f5161
    
    https://github.com/zst123/XuiMod/commit/e3383f127244822b9fc61db169b59e4a502d127b
    
    Credit zst123, thanks for GwonHyeok
    
    Signed-off-by: hany <hany.alsamman@gmail.com>
    
    Conflicts:
    	core/res/res/values/symbols.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3382,6 +3382,7 @@ public final class Settings {
                 "wallpaper_close",
                 "wallpaper_intra_open",
                 "wallpaper_intra_close",
+		"toast_animation",
         };
 
         /**
