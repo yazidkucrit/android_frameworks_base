69f98d55e31f9af8c701d5990285d0004d4df736
TheMrcool212 <munirmohamadrani@gmail.com>
2014-04-03 21:06:11 +0800

    Fix lockscreen wallpaper

--- services/java/com/android/server/power/DisplayPowerController.java
+++ services/java/com/android/server/power/DisplayPowerController.java
@@ -656,14 +656,14 @@ final class DisplayPowerController {
 
                             // scale image if its too large
                             if (bmp.getWidth() > MAX_BLUR_WIDTH) {
-                                tmpBmp = bmp.createScaledBitmap(bmp, MAX_BLUR_WIDTH, MAX_BLUR_HEIGHT, true);
+                                tmpBmp = Bitmap.createScaledBitmap(bmp, MAX_BLUR_WIDTH, MAX_BLUR_HEIGHT, true);
                             }
 
                             mKeyguardService.setBackgroundBitmap(tmpBmp);
                             bmp.recycle();
                             tmpBmp.recycle();
                         }
-                    } else mKeyguardService.setBackgroundBitmap(null);
+                    }
                 }
 
                 mPendingRequestChangedLocked = true;
