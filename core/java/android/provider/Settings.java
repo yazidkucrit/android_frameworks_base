8d76f7cd216bdfd714dec96acb0f88bb87df269e
neighbors28 <sjp0728@gmail.com>
2014-05-09 17:11:10 +0200

    Frameworks/base: Second Clock (1/2)
    https://github.com/Dokdo-Project/platform_frameworks_base/commit/6ab5d6299295246c34a46ffd3f5fe83cdc191cd0#diff-1fa71c270d063a48f921f0760d10c7f3R122
    
    PS2: Fu Whitespace
    PS3: small Issue
    
    Change-Id: Ie04942ae1bb1649d18db51086b46666b56ef1ea0

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3327,6 +3327,12 @@ public final class Settings {
         public static final String STATUSBAR_CLOCK_FONT_STYLE = "statusbar_clock_font_style";
 
         /**
+         * Display second in the Clock
+         * @hide
+         */
+        public static final String CLOCK_USE_SECOND = "clock_use_second";
+
+        /**
          * Quick Settings Quick Pulldown
          *
          * @hide
